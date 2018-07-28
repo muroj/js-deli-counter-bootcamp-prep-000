e376c454c16b42d62e82db2c95b64ceaaa615098
Joe Muro <jmuro0321@gmail.com>
2018-07-28 17:32:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(deliLine, customerName) {
 function nowServing(deliLine) {
   
   if (deliLine.length > 0) {
-    return deliLine.shift();
+    return 'Currently serving ' + deliLine.shift();
   } else {
     return 'There is nobody waiting to be served!'
   }
