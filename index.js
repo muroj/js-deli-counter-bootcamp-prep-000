1ba5f5eb4925b8e818a19cfbc5326ab81951208f
Joe Muro <jmuro0321@gmail.com>
2018-07-28 17:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(deliLine, customerName) {
   deliLine.push(customerName);
   return 'Welcome, ' + customerName + ". You are number " + 
-    (deliLine.length + 1) + ' in line.';
+    (deliLine.length) + ' in line.';
 }
 
 function nowServing(deliLine) {
